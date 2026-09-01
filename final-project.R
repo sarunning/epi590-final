5e04a21df8e78890a0c01ebcffb9cc6d51b8ab4b
Sarah Running <sarahrunning@Uhh-MacBook-Air.local>
2026-09-01 13:31:59 -0400

    created figure

--- final-project.R
+++ final-project.R
@@ -1,13 +1,27 @@
 
 install.packages("gtsummary")
+install.packages("tidytuesdayR")
+install.packages("ggplot2")
+library("gtsummary")
 library("tidytuesdayR")
+library("ggplot2")
 
 #loading in data
 tuesdata <- tidytuesdayR::tt_load('2026-06-09')
 game_films <- tuesdata$game_films
 rm(tuesdata)
 
+#creating summary table of descriptive statistics:
 
+tbl_summary(
+  game_films,
+  by = category,
+  include = c(
+    budget_low, budget_high, category,
+    rotten_tomatoes, metacritic, worldwide_box_office))
 
-
+#making a figure:
+ggplot(data = game_films,
+       aes(x = metacritic, y = rotten_tomatoes)) +
+  geom_point()
 
